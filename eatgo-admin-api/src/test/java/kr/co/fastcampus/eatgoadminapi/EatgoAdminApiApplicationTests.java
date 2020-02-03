d389afded0c875b6d59d422d0fddcd213ca1ee77
WhiteOwl314 <parksj914@naver.com>
2020-02-03 16:52:05 +0900

    Create Project

---
+++ eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgoadminapi/EatgoAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgoadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
